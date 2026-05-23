0e8032e0910d04f1331a8da3f4a0be45b8dae066
PacerguyDon <PacerguyDon@users.noreply.github.com>
2026-05-23 12:14:31 -0600

    Change logo width for The WB brand to 65%

--- src/lib/brands.tsx
+++ src/lib/brands.tsx
@@ -302,7 +302,7 @@ const BRAND_LIBRARY: BrandDefinition[] = [
     kind: 'mask',
     asset: '/logos/the-wb.svg',
     accent: '#002272',
-    logoWidth: '50%',
+    logoWidth: '65%',
     aspectRatio: 248 / 127,
   },
   {
