ee51833d6403f0730fc4ed0f52c93ae1188bf784
PacerguyDon <PacerguyDon@users.noreply.github.com>
2026-05-23 11:53:12 -0600

    Change DC Universe logo width to 20%

--- src/lib/brands.tsx
+++ src/lib/brands.tsx
@@ -95,7 +95,7 @@ const BRAND_LIBRARY: BrandDefinition[] = [
     kind: 'mask',
     asset: '/logos/dc-universe.svg',
     accent: '#020c19',
-    logoWidth: '43%',
+    logoWidth: '20%',
     aspectRatio: 400 / 388,
   },
   {
